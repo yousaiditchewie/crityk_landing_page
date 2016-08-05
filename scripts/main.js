1904abb40d4732aeefe9d159a98ee140ede18495
Jacob Allen <jedijake@me.com>
2016-08-05 10:56:48 -0700

    add jQuery cdn

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,6 @@
 console.log("JS loaded!")
+
+  $(document).ready(function(){
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal-trigger').leanModal();
+  });
