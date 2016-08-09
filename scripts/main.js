b6ffe748cd6363a96bba1d3c31c61d583645f849
Jacob Allen <jedijake@me.com>
2016-08-09 13:52:38 -0700

    add heart svg to aboutus section, add signup form to bottom, add footer

--- scripts/main.js
+++ scripts/main.js
@@ -3,4 +3,5 @@ console.log("JS loaded!")
   $(document).ready(function(){
     // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
     $('.modal-trigger').leanModal();
+    $('.parallax').parallax();
   });
