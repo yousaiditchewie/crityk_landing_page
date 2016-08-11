538d4a7af3f5e53b9d1632c2f812c41e33e57bba
Jacob Allen <jedijake@me.com>
2016-08-11 13:30:52 -0700

    icorporate elevator.js and adjust break section to optimize viewing pleasure

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,19 @@
-console.log("JS loaded!")
+(function(){
 
-  $(document).ready(function(){
-    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
-    $('.modal-trigger').leanModal();
-    $('.parallax').parallax();
+
+  console.log("JS loaded!")
+
+    $(document).ready(function(){
+      // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+      $('.modal-trigger').leanModal();
+      $('.parallax').parallax();
+
+      // ELEVATOR JS VARIABLES AND FUNCTIONS
+      var chevron = document.getElementById("chevron");
+      new Elevator({
+        element: chevron,
+        targetElement: document.querySelector("#aboutus"),
+      });
   });
+
+})();
